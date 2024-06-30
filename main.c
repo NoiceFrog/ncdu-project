f140b2ecc99ce22b5830bda11a4c0d377c413a4f
Ryan Jensen <jensenr30@gmail.com>
2024-06-29 19:41:22 -0500

    Add linked list for directory listing

--- main.c
+++ main.c
@@ -12,6 +12,12 @@ DIR *dir;
 struct dirent *ent;
 int file_count = 0;
 
+struct directory_entry {
+  char file_name[500];
+  uint32_t file_size_bytes;
+  struct directory_entry *next_entry;
+};
+
 int main() {
 
   dir = opendir("."); // open current directory
@@ -24,13 +30,34 @@ int main() {
 
   printf("DT_REG %d\n", DT_REG);
 
+  struct directory_entry my_directory_listing;
+  struct directory_entry *entry = &my_directory_listing;
+
   while ((ent = readdir(dir)) != NULL) {
     file_count++;
     if (ent->d_type == DT_REG) {
       printf("File: %s\n", ent->d_name);
+      strncpy(entry->file_name, ent->d_name, sizeof(entry->file_name));
+      uint32_t file_size = 0; // TODO figure out how to get file size in bytes
+      entry->file_size_bytes = file_size;
+      entry->next_entry = malloc(sizeof(struct directory_entry));
+      entry = entry->next_entry;
+      if (entry == NULL) {
+        printf("PANIC!\n");
+        exit(23);
+      }
+      entry->next_entry = NULL;
+
     } else if (ent->d_type == DT_DIR) {
       printf("Dir: %s\n", ent->d_name);
     }
+  }
+
+  printf("\nFiles in directory:\n");
+  entry = &my_directory_listing;
+  while(entry) {
+    printf("%s\n", entry->file_name);
+    entry = entry->next_entry;
   }
 
   mainU();
